c069234a33e7a9e00e7d13dea496d0df63b17c1a
PREVIRED\prodriguezg <prodriguezg@previred.com>
2018-06-14 15:43:36 -0400

    Agregar colaborador

--- grails-app/domain/proyectohackaton/Solicitud.groovy
+++ grails-app/domain/proyectohackaton/Solicitud.groovy
@@ -5,6 +5,7 @@ class Solicitud {
     TipoSolicitud tipoSolicitud
     Date fechaCreacion
     EstadoSolicitud estado
+    Colaborador colaborador
     
     static hasMany = [flujosSolicitud: FlujoSolicitud] 
     
